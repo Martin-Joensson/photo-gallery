1c4aa9aa0479b40df5f46686e4ed1ec3a0af8090
Martin Jönsson <martin.t.joensson@gmail.com>
2024-12-02 12:23:51 +0100

    Added responsive text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="absolute w-full flex bottom-0 ">
       
-      <p className="font-headerFont"> All images &copy; Martin Jönsson</p>
+      <p className="font-headerFont text-center"> All images &copy; Martin Jönsson</p>
     
       {/* contact information */}
     </div>
