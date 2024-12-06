17cdc99979cf4020217c03220ed6e2d9069f7233
Martin Jönsson <martin.t.joensson@gmail.com>
2024-12-06 12:58:09 +0100

    Cleaned routes

--- src/routes/CategoryRoutes.jsx
+++ src/routes/CategoryRoutes.jsx
@@ -1,9 +1,4 @@
 import { Routes, Route, useParams } from "react-router-dom";
-
-import { Wildlife } from "../pages/Wildlife";
-import { Landscape } from "../pages/Landscape";
-import { Street } from "../pages/Street";
-import { Portrait } from "../pages/Portrait";
 import { Gallery } from "../components/Gallery";
 import { NotFound } from "../pages/NotFound";
 import { Home } from "../pages/Home";
@@ -15,10 +10,6 @@ export const CategoryRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/category/:category" element={<Gallery />} />
-      <Route path="/wildlife" element={<Wildlife />} />
-      <Route path="/landscape" element={<Landscape />} />
-      <Route path="/street" element={<Street />} />
-      <Route path="/Portrait" element={<Portrait />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
   );
